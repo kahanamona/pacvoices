300e76fa68333739ea623e03cad09fc07fda35c1
sione-taumoepeau <sione@caitlah.com>
2022-04-13 11:09:26 +1300

    Fix Memeber

--- pacvoices/pac_voices/doctype/member/member.js
+++ pacvoices/pac_voices/doctype/member/member.js
@@ -5,14 +5,14 @@ frappe.ui.form.on('Member', {
 	refresh: function(frm) {
 		
 		if (!in_list(frappe.user_roles, 'System Manager', 'PAC Voices Admin','PAC Voices Supervisor')){
-			frm.page.sidebar.hide(); // this removes the sidebar
-			$(".form-footer").hide();
+			frm.page.sidebar.hide(); // this hide the sidebar
+			$(".form-footer").hide(); // this hide the footer
 			frm.page.wrapper.find(".layout-main-section-wrapper").addClass("col-md-12");
 		}
 
 		if (in_list(frappe.user_roles, 'System Manager', 'PAC Voices Admin','PAC Voices Supervisor')){
-			frm.page.sidebar.show(); // this removes the sidebar
-			$(".form-footer").show();
+			frm.page.sidebar.show(); // this shows the sidebar
+			$(".form-footer").show();// this shows the footer
 			frm.page.wrapper.find(".layout-main-section-wrapper").addClass("col-md-10");
 		}
 	},
@@ -28,6 +28,7 @@ frappe.ui.form.on('Member', {
 			frm.toggle_enable('full_name', false)
 			frm.toggle_enable('organisation', false)
 			frm.toggle_enable('region', false)
+			frm.toggle_enable('membership_type',false)
 		}
 		if ((frappe.session.user === frm.doc.user)){
 			frm.toggle_enable('user', true)
@@ -37,6 +38,7 @@ frappe.ui.form.on('Member', {
 			frm.toggle_enable('full_name', true)
 			frm.toggle_enable('organisation', true)
 			frm.toggle_enable('region', true)
+			frm.toggle_enable('membership_type',true)
 
 		}
 	}
