5ff9cdf5792a1dd078c0a24d49d88339ac56ef4c
sione-taumoepeau <sione@caitlah.com>
2022-04-13 17:41:26 +1300

    Fix Errors

--- pacvoices/pac_voices/doctype/discussion/discussion_list.js
+++ pacvoices/pac_voices/doctype/discussion/discussion_list.js
@@ -19,17 +19,17 @@ frappe.listview_settings['Discussion'] = {
 	},
     refresh: function(frm) {
 		
-		if (!in_list(frappe.user_roles, 'System Manager', 'PAC Voices Admin','PAC Voices Supervisor')){
-			frm.page.sidebar.hide(); // this removes the sidebar
-			$(".form-footer").hide();
-			frm.page.wrapper.find(".layout-main-section-wrapper").addClass("col-md-12");
-		}
+	//	if (!in_list(frappe.user_roles, 'System Manager', 'PAC Voices Admin','PAC Voices Supervisor')){
+	//		frm.page.sidebar.hide(); // this removes the sidebar
+	//		$(".form-footer").hide();
+	//		frm.page.wrapper.find(".layout-main-section-wrapper").addClass("col-md-12");
+	//	}
 
-		if (in_list(frappe.user_roles, 'System Manager', 'PAC Voices Admin','PAC Voices Supervisor')){
-			frm.page.sidebar.show(); // this removes the sidebar
-			$(".form-footer").show();
-			frm.page.wrapper.find(".layout-main-section-wrapper").addClass("col-md-10");
-		}
+	//	if (in_list(frappe.user_roles, 'System Manager', 'PAC Voices Admin','PAC Voices Supervisor')){
+	//		frm.page.sidebar.show(); // this removes the sidebar
+	//		$(".form-footer").show();
+	//		frm.page.wrapper.find(".layout-main-section-wrapper").addClass("col-md-10");
+	//	}
 	},
 
 }
