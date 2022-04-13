2b77fcc1147c372d2ac2ace3db161a0316213fe5
sione-taumoepeau <sione@caitlah.com>
2022-04-13 17:09:26 +1300

    Fix Discussion Error 2

--- pacvoices/pac_voices/doctype/discussion/discussion.js
+++ pacvoices/pac_voices/doctype/discussion/discussion.js
@@ -29,9 +29,9 @@ frappe.ui.form.on('Discussion', {
 
 	refresh: function(frm) {
 
-		if (!in_list(frappe.user_roles, 'System Manager', 'PAC Voices Admin','PAC Voices Supervisor')){
+		if (in_list(frappe.user_roles, 'PAC Voices Member')){
 			frm.page.sidebar.hide(); // this removes the sidebar
-		//	$(".form-footer").show();
+			$(".form-footer").show();
 			frm.page.wrapper.find(".layout-main-section-wrapper").addClass("col-md-12");
 		}
 
