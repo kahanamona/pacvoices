43fa8ac2beb1cfb734c8dec0bc193dda1c4f64c2
sione-taumoepeau <sione@caitlah.com>
2022-04-14 18:56:06 +1300

    Update Front Page Side

--- README.md
+++ README.md
@@ -5,3 +5,20 @@ Application for the Pacific Regional Civil Society Organisations to record their
 #### License
 
 MIT
+
+#### Try it out
+PAC Voices http://202.134.27.166:8082
+
+PAC Voices Member
+username : bond
+password : 1234
+
+PAC Voices Supervisor
+username : john
+password : 1234
+
+PAC Voices Admin
+username : jane
+paaaword : 1234
+
+Enjoy!
---
+++ pacvoices/www/view_topic.py
@@ -0,0 +1,17 @@
+from __future__ import unicode_literals
+import frappe
+from frappe.utils import now
+from frappe import _
+
+no_cache = 1
+no_sitemap = 1
+
+def get_context(context):
+#    if frappe.session.user == "Guest" and frappe.session.data.user_type !="System User":
+#        frappe.local.flags.redirect_location = "/login"
+#        raise frappe.Redirect
+ 
+
+    context.discussion = frappe.db.sql("select name, title, topic, discussion_content, published_date, owner, status from `tabDiscussion` where published = 1", as_dict=True)
+#    context.intranet_header = frappe.db.sql("select intranet_title, file_attach from `tabIntranet Header` where published = 1", as_dict=True)
+#    context.document_manager = frappe.db.sql("select owner, modified, sub_menu, file_url, file_name, top_menu from `tabFile` where published = 1 and is_private = 0", as_dict=True)
